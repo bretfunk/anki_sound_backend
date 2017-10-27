2db0ea6c84a21b192b7f3274c340bae6b9179ea2
BFunk <bret.e.funk@gmail.com>
2017-10-26 20:55:52 -0600

    cors

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,13 @@ module AnkiSoundBackend
     config.api_only = true
   end
 end
+module YourApp
+  class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,13 +6,3 @@
 #     https: false
 #   )
 # end
-module YourApp
-  class Application < Rails::Application
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-  end
-end
