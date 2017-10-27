ef2a9c8c61e39741c7846c01c2885e61146775f9
BFunk <bret.e.funk@gmail.com>
2017-10-26 20:51:29 -0600

    cors

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,11 +6,13 @@
 #     https: false
 #   )
 # end
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
+module YourApp
+  class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
-           end
-   end
