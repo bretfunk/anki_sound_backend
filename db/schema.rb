191972129a70b56f93997f567315b548d2e25cd2
BFunk <bret.e.funk@gmail.com>
2017-10-30 18:06:56 -0600

    updated seed file

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20171029152259) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
