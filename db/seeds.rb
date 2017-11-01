078e9db2d48ddd5e86c2da8056edeef420bb9f99
BFunk <bret.e.funk@gmail.com>
2017-10-31 21:07:16 -0600

    updating a horrible beatles song seeding error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 EMAIL = ['john@beatles.com', 'paul@beatles.com', 'george@beatles.com', 'ringo@beatles.com']
-PHRASES = ['Help', 'Elenor Rigby', 'I want to hold your hand', 'All you need is love', 'Dr. Robert', 'Buffalo Bill', 'Abby Road', 'Blue Jay Way']
+PHRASES = ['Help', 'Elenor Rigby', 'I want to hold your hand', 'All you need is love', 'Dr. Robert', 'Bungalo Bill', 'Abby Road', 'Blue Jay Way']
 EMAIL.map do |email|
   new_user = User.create(email: email, password: "Test")
   3.times { new_user.phrases << Phrase.create(phrase: PHRASES.sample, language: "English") }
+  puts("user #{email} created!")
+end
 
 User.create(email: 'bret@bret', password: "test")
 User.last.phrases << Phrase.first
 User.last.phrases << Phrase.last
-end
+puts("user #{User.last.email} created!")
 
 
 
