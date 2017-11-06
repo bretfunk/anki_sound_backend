6a38fab07deafe5a2f9a0f58add35a5d8163d09a
BFunk <bret.e.funk@gmail.com>
2017-11-05 20:55:56 -0700

    herkou password debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def user_params
-    params.require(:user).permit(:email, :password, :admin)
+    params.permit(:email, :password, :admin)
   end
 end
