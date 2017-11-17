e3eeba1870eb08a2094a349d2ca39a07937109e1
BFunk <bret.e.funk@gmail.com>
2017-11-17 09:51:07 -0700

    backend download and storing improvements

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -5,6 +5,7 @@ class AudioController < ApplicationController
   def index
     phrase = params[:phrase]
     language = params[:language]
-    Audio.save_file(phrase, language)
+    file_name = params[:file_name]
+    Audio.save_file(phrase, language, file_name)
   end
 end
--- app/models/audio.rb
+++ app/models/audio.rb
@@ -2,7 +2,6 @@ require 'tts'
 
 class Audio < ApplicationRecord
   def self.save_file(phrase, language, file_name)
-    "#{phrase}".to_file "#{language}", "./public/#{file_name}"
-    #"Light rain with highs of 5 degrees".to_file "en", "./public/weather.mp3"
+    "#{phrase}".to_file "#{language}", "./public/#{language}/#{file_name}.mp3"
   end
 end
