510a21ec04db70cc67b56e2fdfc1414dbfb32a7f
BFunk <bret.e.funk@gmail.com>
2017-11-25 12:19:20 -0700

    trying to fix the multi sound file bug

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -2,6 +2,9 @@ require 'tts'
 
 class Audio < ApplicationRecord
   def self.save_file(phrase, language, file_name)
-    "#{phrase}".to_file "#{language}", "./public/#{language}/#{file_name}.mp3"
+    if File.file?("./public/#{language}/#{file_name}.mp3")
+    else
+      "#{phrase}".to_file "#{language}", "./public/#{language}/#{file_name}.mp3"
+    end
   end
 end
