001c230331e90043aa4bfdddb0c55c511d37282f
BFunk <bret.e.funk@gmail.com>
2017-12-05 12:17:54 -0500

    final changes

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -3,11 +3,9 @@ require 'tts'
 class Audio < ApplicationRecord
   def self.save_file(phrase, language, file_name)
     if File.file?("./public/#{language}/#{file_name}.mp3")
-      #move to controller
       AwsService.upload(language, file_name)
     else
       "#{phrase}".to_file "#{language}", "./public/#{language}/#{file_name}.mp3"
-      #move to controller
       AwsService.upload(language, file_name)
     end
   end
--- app/services/aws_service.rb
+++ app/services/aws_service.rb
@@ -4,9 +4,7 @@ class AwsService
 
   def self.upload(language, file_name)
     s3 = Aws::S3::Resource.new(region:"us-west-2")
-    obj = s3.bucket("elasticbeanstalk-us-west-2-747213477632").object("/#{language}/#{file_name}.mp3")
-    #obj = s3.bucket("elasticbeanstalk-us-west-2-747213477632").object("/en/i_hate_you.mp3")
-    #obj.upload_file("./public/en/i_hate_you.mp3")
+    obj = s3.bucket("elasticbeanstalk-us-west-2-747213477632").object("#{language}/#{file_name}.mp3")
     obj.upload_file("./public/#{language}/#{file_name}.mp3")
   end
 end
